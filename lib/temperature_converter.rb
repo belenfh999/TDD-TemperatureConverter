307047a109d61640021a3ccc1e41772d036f6428
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-21 10:04:29 +0200

    Adding minimal implementation for conversion: Celsius -> Fahrenheit

--- lib/temperature_converter.rb
+++ lib/temperature_converter.rb
@@ -1,7 +1,11 @@
 module TemperatureConverterMod
   class TemperatureConverter
     def convert_temp(temperature, input_scale:, output_scale: 'celsius')
+      if input_scale == 'celsius' and output_scale == 'fahrenheit'
+        (temperature * 1.8) + 32
+      else
       temperature
+      end
     end
   end
 end
