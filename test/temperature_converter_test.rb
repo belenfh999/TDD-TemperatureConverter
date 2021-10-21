ca3c31f1beee678a66bee515e8ae2e467153c7a6
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-21 09:57:47 +0200

    Added other conversion tests from and to the same scale

--- test/temperature_converter_test.rb
+++ test/temperature_converter_test.rb
@@ -10,4 +10,14 @@ class TemperatureConverterTest < Minitest::Test
     expected_temperature = 20
     assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'celsius'))
   end
+
+  def test_kelvin_to_kelvin
+    expected_temperature = 20
+    assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'kelvin', output_scale: 'kelvin'))
+  end
+
+  def test_fahrenheit_to_fahrenheit
+    expected_temperature = 20
+    assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'fahrenheit', output_scale: 'fahrenheit'))
+  end
 end
