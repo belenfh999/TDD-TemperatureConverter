0efc5689a300c9beb4e5d10e618c417b0ef62f9f
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-21 10:14:50 +0200

    Refactor tests with input temperature for readability

--- test/temperature_converter_test.rb
+++ test/temperature_converter_test.rb
@@ -7,24 +7,28 @@ class TemperatureConverterTest < Minitest::Test
   end
 
   def test_celsius_to_celsius
+    input_temperature = 20
     expected_temperature = 20
-    assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'celsius'))
+    assert_equal(expected_temperature, @temperature_converter.convert_temp(input_temperature, input_scale: 'celsius'))
   end
 
   def test_kelvin_to_kelvin
+    input_temperature = 20
     expected_temperature = 20
-    assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'kelvin', output_scale: 'kelvin'))
+    assert_equal(expected_temperature, @temperature_converter.convert_temp(input_temperature, input_scale: 'kelvin', output_scale: 'kelvin'))
   end
 
   def test_fahrenheit_to_fahrenheit
+    input_temperature = 20
     expected_temperature = 20
-    assert_equal(expected_temperature, @temperature_converter.convert_temp(20, input_scale: 'fahrenheit', output_scale: 'fahrenheit'))
+    assert_equal(expected_temperature, @temperature_converter.convert_temp(input_temperature, input_scale: 'fahrenheit', output_scale: 'fahrenheit'))
   end
 
   def test_celsius_to_fahrenheit
+    input_temperature = 0
     expected_temperature = 32.0
     assert_equal(expected_temperature,
-                 @temperature_converter.convert_temp(0, input_scale: 'celsius', output_scale: 'fahrenheit'))
+                 @temperature_converter.convert_temp(input_temperature, input_scale: 'celsius', output_scale: 'fahrenheit'))
   end
 
   def test_fahrenheit_to_celsius
